c54f04527cd97849f70ff26819a143a797d9d548
i-tanaka730 <pttp730@gmail.com>
2021-05-24 23:29:07 +0900

    feat: staticに変更、及びコメント追加。

--- src/microcms.js
+++ src/microcms.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
 
-export class MicroCms {
-  async getData (url, apikey) {
+/**
+ * microCMSのサービスクラス。
+ */
+export default class MicroCms {
+
+  /**
+   * 指定したURL/APIキーを元に、microCMSからデータを取得する。
+   */
+  static async getData (url, apikey) {
     const headers = { headers:{ "X-API-KEY": apikey } };
     const  { data } = await axios.get(url, headers);
     return data.contents;
   }
+
 }
