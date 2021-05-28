6aa7c34974b19c73c4a18065dd4cbd737982fe80
i-tanaka730 <pttp730@gmail.com>
2021-05-28 17:22:48 +0900

    feat: TypeScriptのコードに変更。

--- src/microcms.ts
+++ src/microcms.ts
@@ -8,7 +8,7 @@ export default class MicroCms {
   /**
    * 指定したURL/APIキーを元に、microCMSからデータを取得する。
    */
-  static async getData (url, apikey) {
+  static async getData (url:string, apikey:string) {
     const headers = { headers:{ "X-API-KEY": apikey } };
     const  { data } = await axios.get(url, headers);
     return data.contents;
